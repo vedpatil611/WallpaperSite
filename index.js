57a067f8b4aef56310f78ed323c23715d645c9c2
vedpatil611 <vedpatil611@gmail.com>
2020-08-03 09:42:19 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const ejs = require('ejs');
+const upload = multer({ dest: 'uploads/'});
+var fs = require('fs');
+
+
+var database = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "wallpaper"
+});
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+// app.use(methodOverride("_method"));
+
+app.get("/", function (req, res) {
+	res.render("home");
+});
+
+app.get("/upload", function (req, res) {
+	res.render("upload");
+});
+
+app.post("/upload", upload.single('path'), function (req, res) {
+	var title = req.body.title;
+	temp = __dirname + "/" + req.file.path;
+	var path = pathf.replace(/\\/g, "/");
+	console.log(path);
+
+	database.query("INSERT INTO images(title, data) VALUES(\"" + title + "\", LOAD_FILE(\"" + path + "\"))");
+	res.redirect("/");
+});
+
+app.get("/show", function(req, res) {
+	database.query("SELECT data FROM images", function(err, result) {
+		// res.send(result);
+		obj = JSON.parse(JSON.stringify(result));
+		console.log(obj);
+		res.render("show", {obj: obj});
+	});
+});
+
+app.listen(8000, function() {
+	console.log("Hello there");
+});
